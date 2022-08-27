1863661d406e57b40fe5a706cb398331a2ee4256
derickdsilva <derickdss38@gmail.com>
2022-08-27 17:57:25 +0100

    cleanup

--- App.js
+++ App.js
@@ -28,7 +28,6 @@ export default function App() {
             headerTitleStyle: {
               fontWeight: 'bold',
             },
-            operation: "addition"
           }}
         />
         <Stack.Screen
